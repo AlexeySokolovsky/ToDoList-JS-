1fd198069c05396f5c0a54d2bc39059b92d545ca
AlexeySokolovsky <alexey.sokoloffsky@gmail.com>
2023-01-22 19:44:36 +0300

    full update all bugfix

--- js/script.js
+++ js/script.js
@@ -1,49 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const toDoEvents = {
-		events: []
-	};
-
-
 	const addInput = document.querySelector('.add-input'),
 		addForm = document.querySelector('form.add'),
 		listEvents = document.querySelector('.todolistmain');
 	addForm.addEventListener('submit', (event) => {
 		event.preventDefault();
-		let newEvent = addInput.value;
-		if (newEvent.length > 37) {
-			newEvent = `${newEvent.substring(0, 38)}...`;
-		}
-		if (newEvent) {
-			if (toDoEvents.events.length < 7) {
-				toDoEvents.events.push(newEvent);
-				event.target.reset();
-				addEvents(toDoEvents.events, listEvents);
-			
+		let value = addInput.value;
+		if (value) {
+			if (listEvents.childNodes.length < 8) {
+				if (value.length > 32) {
+					value = `${value.substring(0, 32)}...`;
+
+				}
+				addToList(value, listEvents);
 			}
 		}
+		event.target.reset();
+
 	});
 
-
-	function addEvents(events, parent) {
-		parent.innerHTML = '';
-		events.forEach((event, i) => {
-			parent.innerHTML += `<li class="todolistli">${i+1}. ${event}<div class="apply"></div><div class="delete"></div></li>`;
-		});
-		document.querySelectorAll('.delete').forEach((btn, i) => {
-			btn.addEventListener('click', () => {
-				btn.parentElement.remove();
-				toDoEvents.events.splice(i, 1);
-				addEvents(events, parent);
+	let addToList = (item, list) => {
+		list.innerHTML += `<li class="todolistli">${item}<div class="apply"></div><div class="delete"></div></li>`;
+		document.querySelectorAll('.delete').forEach((elem, i) => {
+			elem.addEventListener('click', () => {
+				elem.parentElement.remove();
 			});
 		});
-		document.querySelectorAll('.apply').forEach((btn, i) => {
-			btn.addEventListener('click', () => {
-				btn.parentElement.style.color = '#41B3A3';
+		document.querySelectorAll('.apply').forEach((elem, i) => {
+			elem.addEventListener('click', () => {
+				elem.parentElement.style.color = '#41B3A3';
 			});
 		});
 
+	};
 
-		addEvents(listEvents);
-	}
 });
+
 
